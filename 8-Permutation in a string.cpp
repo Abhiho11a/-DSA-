6e3ed13ab45537318ede403d199d185dd985cc4c
Abhiho11a <abhishekholla01@gmail.com>
2024-12-03 17:59:34 +0530

    Add files via upload
    https://leetcode.com/problems/permutation-in-string/submissions/1469210126

---
+++ 8-Permutation in a string.cpp
@@ -0,0 +1,62 @@
+//PROGRAM TO CHECK WHETHER THE PERMUTATION OF AN ENTERED STRING WILL BE IN THE OTHER ONE 
+#include<iostream>
+using namespace std;
+
+
+bool isfreq(int fre1[],int fre2[])   //COMPARING 2 STRINGS USING ARRAY CONCEPT
+{
+    for(int i=0;i<26;i++)
+    {if(fre1[i]!=fre2[i])
+    {
+        return false;
+    }}
+    return true;
+}
+
+
+
+bool check(string s1,string s2)//GETTING ALL THE CHARACTERS IN A STRING USING ARRAY
+  {
+    int f[26]={0};
+    for(int i=0;i<s1.length();i++)
+    {
+        f[s1[i]-'a']++;//1ST STRING
+
+    }
+
+
+    int bxsize=s1.length();
+    for(int i=0;i<s2.length();i++)
+    {
+        int bxind=0,
+        ind=i;
+        int bxfreq[26]={0};
+
+        while(bxind<bxsize &&  ind<s2.length())
+        {
+            bxfreq[s2[ind]-'a']++;//2ND STRING 
+            bxind++;
+            ind++;
+        }
+    
+       if(isfreq( f,bxfreq))//CALLING FUNCTION
+       {
+        return true;
+       }
+       
+    } 
+    return false;
+    }
+
+
+int main()
+{
+  std:string s1,s2;
+  cout<<"enter a substring to check ";
+  cin>>s1;
+  cout<<"enter the string ";
+  cin>>s2;
+  cout<<check(s1,s2);
+
+}
+
