f65b5458dd8e5cd177251a7f59b2f05bffbd2335
Abhiho11a <abhishekholla01@gmail.com>
2024-12-07 23:51:51 +0530

    Add files via upload
    https://leetcode.com/problems/two-sum/submissions/1472831280

---
+++ 13-Two sum.cpp
@@ -0,0 +1,39 @@
+//TO FIND 2 NUMBERS WHOSE SUM ISEQUAL TO THE TARGET
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+int main()
+{
+    int n,target;
+    int ans1,ans2=0;
+    vector<int>ans;                                     //USING CONCEPT OF HASHING
+    unordered_map<int,int>box;
+    int arr[50];
+    cout<<"Enter the number of elements in an array: ";
+    cin>>n;
+    cout<<"enter array elements: ";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cou<<"Enter target sum: ";
+    cin>>target;
+
+
+    for(int i=0;i<n;i++)
+    {
+        int first=arr[i];
+        int second=target-first;
+
+        if(box.find(second)!=box.end())
+        {
+            ans2=i;
+            ans1=box[second];
+            break;
+        }
+        box[first]=i;
+    }
+
+    cout<<"Add the element present in index "<<ans1<<" and "<<ans2<<" to get ans is equal to "<<target;
+}
